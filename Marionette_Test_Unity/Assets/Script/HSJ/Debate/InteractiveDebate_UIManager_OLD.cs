212c661c55e2251b52d1cd8783117e65ce9e1ef4
HSangJin <sdit111@gmail.com>
2025-08-12 15:47:02 +0900

    리소스 메타 수정 푸쉬, 에러 주석

--- Marionette_Test_Unity/Assets/Script/HSJ/Debate/InteractiveDebate_UIManager_OLD.cs
+++ Marionette_Test_Unity/Assets/Script/HSJ/Debate/InteractiveDebate_UIManager_OLD.cs
@@ -188,7 +188,7 @@ public class InteractiveDebate_UIManager_OLD : MonoBehaviour
         {
             ItemSlot itemSlot = Instantiate(itemSlotPrefab, inventoryViewer);
             //일단 임시
-            itemSlot.AddItem(item.Key.ToString(), item.Value, null, "");
+            //itemSlot.AddItem(item.Key, item.Value, null, "");
             itemSlots.Add(itemSlot.gameObject);
         }
     }
